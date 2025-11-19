5dd9fc3660c6f296d1556dc7697a95e19e1e4c02
d3m0n0fth3d4yl1ght <renato.sorrentino@outlook.com>
2025-11-19 16:37:54 -0300

    19-11-1

--- projeto/script.js
+++ projeto/script.js
@@ -11,3 +11,11 @@ const galeria = documento.getElementById("galeriaHero");
 
 const imagemInicial = chips [0].dataset.imagem;
 galeria.style.backgroundImage = `url()`
+
+// forEach para cada objeto repetir
+chips.forEach(chip) -> {
+    chip.addEventListener("click", ()) => {
+        galeria.classlist.add("ativa");
+        galeria.style.backgroundImage = 'url(${chip.dataset.image})';
+    }
+}
